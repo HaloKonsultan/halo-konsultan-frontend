3047ad26f923ee46f58640bc163941c02048f128
Mahiraardelia <mahiraardelia26@gmail.com>
2021-11-29 18:54:00 +0700

    Layouting Send Message

--- src/components/pages/Message/SendMessage.js
+++ src/components/pages/Message/SendMessage.js
@@ -3,7 +3,7 @@ import Nav from "../../layout/Header";
 import { Input, Space, Card, Typography, Layout} from 'antd';
 import {SendOutlined} from "@ant-design/icons";
 import Cookies from "js-cookie";
-
+import {PaperPlaneRight} from "phosphor-react";
 
 const { Footer } = Layout;
 const { Text} = Typography;
@@ -11,18 +11,20 @@ const SendMessage = () => {
 
     return (
         <>
-        <Layout>
-        <Footer style={{backgroundColor: "white"}}>
-        <Card style={{ width: 92, height:40, borderRadius: 20 }}>
-                 <h5 style={{color: "gray"}}>
+        <Layout style={{backgroundColor : "white"}}>
+        <Space size={500} direction="vertical"> 
+        {/* <div className="profile-label">    */}
+        <Card style={{ width: 92, height:40, borderRadius: 20, }}>
+                 {/* <h5 style={{color: "gray"}}>
                               6/6/21
-                            </h5>
+                            </h5> */}
             </Card>
-            <Input style={{borderRadius: 26, height: 48 }}
-                    placeholder="Tulis Pesanmu disini"
-                     /></Footer>
+           
+            <Input style={{borderRadius: 26, height: 56}}
+                    placeholder="Tulis Pesanmu disini" 
+                    prefix={<PaperPlaneRight size={24} color="#3B85FA"/>}
+                     /></Space>
         </Layout>
-         
           
         </>
     )
