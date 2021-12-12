978392fe9306194dea93233b3f638fb4c7842658
Mahiraardelia <mahiraardelia26@gmail.com>
2021-12-12 18:27:55 +0700

    Responsive Message

--- src/components/layout/HeaderMessage.js
+++ src/components/layout/HeaderMessage.js
@@ -1,14 +1,39 @@
 import React, {useContext, useState} from "react"
-import {useHistory, useLocation} from "react-router-dom"
+import {useHistory} from "react-router-dom"
 import Cookies from "js-cookie";
-import {Button, Layout, PageHeader, Row, Col, Modal, Typography, Space} from 'antd';
-import {ContextProfile} from "../context/ContextProfile"
-
+import Logo from "../../assets/img/logo.png";
+import {Button, Layout, PageHeader, Row, Col, Space, Typography, Drawer, Menu, Modal, Form} from 'antd';
+import {CirclesFour, ChatCenteredDots, ClockCounterClockwise, User, List} from "phosphor-react";
+import {ContextMessage} from "../context/ContextMessage"
+import {SignOut} from "phosphor-react";
+import API from "../context/API";
+import ButtonDanger from "../global/ButtonDanger"
+import PrimaryButton from "../global/ButtonPrimary"
+import {ContextProfile} from "../context/ContextProfile";
+import {ContextUser} from "../context/ContextUser";
+import {SERVER_NAME} from "../context/API";
+import noImage from "../../assets/img/no-image.png";
 const {Text, Title} = Typography;
+const {Sider} = Layout;
+
 const NavMessage = (props) => {
+    let history = useHistory()
     const {functions} = useContext(ContextMessage)
+    const {input, setInput} = useContext(ContextProfile)
+    const {loginStatus, setLoginStatus} = useContext(ContextUser)
     const {functionEndForum} = functions
     const [isEndForum, setIsEndForum] = useState(false);
+
+    const handleLogout = () => {
+        setLoginStatus(false)
+        Cookies.remove('token')
+        Cookies.remove('id')
+        Cookies.remove('location')
+        API.post(`consultants/logout`, {},
+            {headers: {"Authorization": "Bearer " + Cookies.get('token')}}
+        )
+        history.push('/')
+    }
 
     const showModal = () => {
         setIsEndForum(true);
@@ -23,12 +48,40 @@ const NavMessage = (props) => {
         setIsEndForum(false);
     };
     
+    const handleMenu = (event) => {
+        console.log(event.key)
+        switch (event.key) {
+            case "1":
+                return history.push('/')
+                break;
+            case "2":
+                return history.push('/message')
+                break;
+            case "3":
+                return history.push('/history')
+                break;
+            case "4":
+                return history.push('/profile')
+                break;
+        }
+    }
+
+    const [visible, setVisible] = useState(false);
+    const showDrawer = () => {
+        setVisible(true);
+    };
+    const onClose = () => {
+        setVisible(false);
+    };
 
     return (
         <>
             <Layout>
             {
              Cookies.get('token') !== undefined &&
+             <>
+                 <Row>
+                 <Col xs={{span: 0, order: 2}} sm={{span: 0, order: 2}} lg={{span: 24, order: 1}}>
                     <PageHeader
                         onBack={props.onBack}
                         style={{backgroundColor: "white", borderBottom: " 1px solid #CED4DA", width:"100%"}}
@@ -44,7 +97,81 @@ const NavMessage = (props) => {
                           </Button>
                         ]}
                     />
-                }
+                </Col>
+                <Col xs={{span: 24, order: 1}} sm={{span: 24, order: 1}} lg={{span: 0, order: 2}}>
+                                <PageHeader
+                                    onBack={props.onBack}
+                                    style={{backgroundColor: "white"}}
+                                    title={<img src={Logo} style={{width: 150}} alt=""/>}
+                                    extra={[
+                                    <Button type="text" onClick={showDrawer}>
+                                        <List size={24} />
+                                    </Button>
+                                    ]}
+                                />
+                            </Col>
+                            <Col xs={{span: 24, order: 1}} sm={{span: 24, order: 1}} lg={{span: 0}} style={{backgroundColor: "white", marginTop: 0}}>
+                                <div style={{backgroundColor: "white", margin: "16px 32px 16px 32px", display: props.mobile}}>
+                                    <Space direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
+                                        <Text style={{justifyContent: "center", fontWeight: 700, fontSize: 24}}>{props.mobileTitle}</Text>
+                                    </Space>
+                                </div>
+                            </Col>
+                        </Row>
+
+                         <Drawer 
+                         placement="right"
+                         onClose={onClose} 
+                         visible={visible}
+                         >
+                         <Space style={{marginTop: 32, marginBottom: 32}}>
+                             {
+                                 input.photo === SERVER_NAME + null &&
+                                 <img
+                                     src={noImage}
+                                     alt="profile-picture"
+                                     style={{
+                                         width: 40,
+                                         height: 40,
+                                         objectFit: "cover",
+                                         borderRadius: "50%",
+                                         marginLeft: "24px",
+                                     }}/>
+                                 ||
+                                 <img src={input.photo} className="people" alt="" style={{
+                                     width: 40,
+                                     height: 40,
+                                     objectFit: "cover",
+                                     borderRadius: "50%",
+                                     marginLeft: "24px",
+                                 }}/>
+                             }
+                             <Space size={24} direction="vertical">
+                                 <Space size={4} direction="vertical">
+                                     <Text strong>{input.name}</Text>
+                                     <Text type="secondary">{input.position}</Text>
+                                 </Space>
+                             </Space>
+                         </Space>
+                        <Menu
+                             mode="inline" s
+                             defaultSelectedKeys={['1']}
+                             style={{color: "#B0B0B0"}}
+                         >  
+                             <Menu.Item key="1" icon={<CirclesFour size={24} weight="fill"/>}
+                                     onClick={handleMenu}>Beranda</Menu.Item>
+                             <Menu.Item key="2" icon={<ChatCenteredDots size={24} weight="fill"/>}
+                                     onClick={handleMenu}>Pesan</Menu.Item>
+                             <Menu.Item key="3" icon={<ClockCounterClockwise size={24} weight="fill"/>}
+                                     onClick={handleMenu}>Riwayat</Menu.Item>
+                             <Menu.Item key="4" icon={<User size={24} weight="fill"/>}
+                                     onClick={handleMenu}>Profil</Menu.Item>
+                             <Menu.Item key="5" type="link" icon={<SignOut size={24} weight="fill"/>}
+                                     onClick={handleLogout} danger>Keluar</Menu.Item>
+                         </Menu>
+                     </Drawer>
+                    </>
+                 }
 
                 <Modal
                     className="profile-modal"
@@ -79,10 +206,13 @@ const NavMessage = (props) => {
                         </Form.Item>
                     </Form>
                 </Modal>
-            
             </Layout>
         </>
     )
 }
+
+NavMessage.defaultProps = {
+    mobile: "none",
+};
 
 export default NavMessage
--- src/components/pages/Message/Message.js
+++ src/components/pages/Message/Message.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Row, Col} from 'antd';
 import Nav from "../../layout/Header";
 import HomeMessage from "./HomeMessage";
 import SendMessage from "./SendMessage";
-// import NavMessage from "../../layout/HeaderMessage";
+import NavMessage from "../../layout/HeaderMessage";
+import {ContextMessage} from "../../context/ContextMessage";
 
 const Message = () => {
 
+const {userName} = useContext(ContextMessage)
     return (
         <>
             {/* <Nav title="Pesan" mobileTitle="Pesan" mobile="true"/> */}
@@ -20,7 +22,7 @@ const Message = () => {
                      </div>
                </Col>
                <Col xs={{span: 24, order: 1}} sm={{span: 24, order: 1}} lg={{span: 12, order: 1}}>
-               <Nav title="Pesan" mobileTitle="Pesan" mobile="true" />
+               <NavMessage title={userName} mobileTitle="Name" mobile="true"/>
                    <div className="container-profile"  style={{alignItems: "center", display: "flex"}}>
                        <SendMessage/>
                    </div>
